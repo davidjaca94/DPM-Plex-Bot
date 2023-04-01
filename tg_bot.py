7790b5d357c9b34c5af9f7eabc6728bc0d4cf899
davidjaca94 <davidjaca94@gmail.com>
2023-04-01 03:07:05 +0200

    Adapt skeleton, configure handlers and user filter

--- tg_bot.py
+++ tg_bot.py
@@ -1,99 +1,59 @@
+﻿# Native imports
 import os
 import json
-import time
 import shutil
 import logging
-import argparse
-from uuid import uuid4
 
+# Installed imports
+import requests
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedPhoto, ParseMode, InputMediaPhoto
 from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, InlineQueryHandler, ChosenInlineResultHandler, Filters
 from telegram.ext.dispatcher import run_async
 
+# Custom imports
+from tg_conf import *
 
-tg_bot_token = "" # https://t.me/...
-tg_group_id = "" # https://t.me/joinchat/...
 
-watermark_path = "watermark.png"
-images_path = "images/"
-latents_path = "latents/"
-polls_path = "polls.json"
-photos_map_path = "photos.json"
-results_path = "results.json"
-cache_paths = [images_path, latents_path]
-data_paths = [polls_path, photos_map_path, results_path]
-
-reset_types = ['all', 'cache', 'data']
-
-commands_1 = ["Joven", "Viejo", "Hombre", "Mujer"]
-commands_2 = ["Fusion"]
-commands = {'1': commands_1, '2': commands_2, 'all': commands_1+commands_2}
-
-commands_map ={'Joven': {'cmd': "age", 'sign': -1}, 'Viejo': {'cmd': "age", 'sign': 1}, 'Hombre': {'cmd': "gender", 'sign': 1}, 'Mujer': {'cmd': "gender", 'sign': -1}}
-
-poll_options = {'like': "👍", 'dislike': "👎", 'indifferent': "😐", 'scary': "😱", 'funny': "😂", 'lovely': "😍"}
-empty_options = {option: [] for option in poll_options}
+# Global vars
+tg_users = lambda: [int(user) for user in list(set(json.loads(open("tg_users.json", "r").read()).values()))]
 
 # Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
+logging.basicConfig(format="[%(asctime)s] [%(levelname)s] %(message)s", level=logging.INFO)
+logging.getLogger('apscheduler').setLevel(logging.CRITICAL)
+logging.getLogger('telegram').setLevel(logging.CRITICAL)
 
 
-#region GAN functions
-def encode_photos(input_photos_path):
-    encoded_photos_path = [input_photo_path.replace(images_path, latents_path).replace(".jpg", ".npy") for input_photo_path in input_photos_path]
-    new_photo_paths = [input_photo_path for input_photo_path, encoded_photo_path in zip(input_photos_path, encoded_photos_path) if not os.path.exists(encoded_photo_path)]
-    
-    os.makedirs(latents_path, exist_ok=True)
-    
-    gan_methods.images_align(new_photo_paths, new_photo_paths)
-    gan_methods.images_vectorize(new_photo_paths, latents_path)
+#region TheMovieDB auxiliary functions
 
-    return encoded_photos_path
-
-def process_photos(input_photos_id, output_photo_id, command):
-    input_photos_path = [f"{images_path}{input_photo_id}.jpg" for input_photo_id in input_photos_id]
-    output_path = f"{images_path}{output_photo_id}.jpg"
-    result = {'path': output_path}
-
-    try:
-        if not os.path.exists(output_path):
-            if not facade_mode:
-                encoded_photos_path = encode_photos(input_photos_path)
-
-                if command=="Fusion":
-                    gan_methods.faces_mix(encoded_photos_path, output_path)
-                elif command in commands_1:
-                    command_map = commands_map[command]
-                    lvl = 3 * command_map['sign']
-                    cmd = command_map['cmd']
-                    gan_methods.face_style(encoded_photos_path[0], cmd, lvl, output_path)
-                else:
-                    # Command defined but not implemented
-                    raise Exception(f"Funcionalidad '{command}' en desarrollo...")
-                
-                if watermark_enabled:
-                    if os.path.exists(output_path):
-                        if os.path.exists(watermark_path):
-                            gan_methods.burn_watermark(output_path, watermark_path, (524, 933))
-            else:
-                shutil.copy(input_photos_path[0], output_path)
-
-    except Exception as e:
-        logger.error(e)
-        result['error'] = e
-
-    # Clean temp files
-    for input_photo_path in input_photos_path:
-        if os.path.exists(input_photo_path):
-            os.remove(input_photo_path)
-
-    return result
+pass
 
 #endregion
 
 
-#region Auxiliary functions
+#region TheMovieDB call functions
+
+def search_movies(query):
+    language = 'es-ES'
+    url = f'https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={query}&language={language}'
+    response = requests.get(url)
+    data = response.json()
+
+    results = data['results']
+    if results:
+        for result in results:
+            title = result['title']
+            release_date = result['release_date']
+            print(f'{title} ({release_date})')
+    else:
+        print('No se encontraron resultados para esta b�squeda.')
+
+
+#endregion
+
+
+#region Telegram auxiliary functions
+
 def generate_photo_menu(msg_id, photos, cmd_type):
     keyboard = []
 
@@ -131,9 +91,9 @@ def parse_photo_id(tg_photo_id):
 def unparse_photo_id(photo_id):
     photos_map = json.loads(open(photos_map_path, "r").read()) if os.path.exists(photos_map_path) else {}
     inverse_map = {v: k for k, v in photos_map.items()}
-    
+
     tg_photo_id = inverse_map[photo_id] if photo_id in inverse_map else None
-    
+
     return tg_photo_id
 
 def get_photo(update):
@@ -248,10 +208,24 @@ def remove_path(path):
             # Delete folder with all content
             shutil.rmtree(path, ignore_errors=True)
 
+def tg_logger_format(update, text):
+    log = ""
+
+    log += f"[{update.message.chat.title}] " if update.message.chat.title else ""
+
+    log += f"[{update._effective_user.id}]"
+    log += f"[@{update._effective_user.username}]" if update._effective_user.username else ""
+    log += f"[{update._effective_user.first_name} {update._effective_user.last_name}] ".replace(" None", "")
+
+    log += text
+
+    return log
+
 #endregion
 
 
-#region Handler functions
+#region Telegram handler functions
+
 def start(update, context):
     msg = '''
 • *Mándame 1 foto* frontal de tu cara y podrás hacer lo siguiente:
@@ -268,31 +242,52 @@ def start(update, context):
 def help(update, context):
     start(update, context)
 
-def photo(update, context):
-    chat_id = update.message.chat.id
-    msg_id = update.message.message_id
-    user_id = update._effective_user.id
-    user_name = f"{update._effective_user.first_name} {update._effective_user.last_name}".replace(" None", "")
+def bad_msg(update, context):
+    if update._effective_chat.type=="private":
+        logger.warning(tg_logger_format(update, f"Invalid command '{update.message.text}' {update}"))
+        update.message.reply_text("Lo siento pero sólo entiendo klingon y una serie de comandos")
+        start(update, context)
 
-    photo_id = get_photo(update)
-    photos = get_album(update, context, photo_id)
-    
-    if len(photos)==1:
-        reply_markup = generate_photo_menu(msg_id, photos, '1')
-        reply = update.message.reply_text("¿Qué quieres hacer con esta foto?", reply_markup=reply_markup)
+def bad_user(update, context):
+    if update._effective_chat.type == "private":
+        logger.warning(tg_logger_format(update, f"Forbidden user {update}"))
+        update.message.reply_text("No eres digno de empuñar a Excalibur, si crees que es un error contacta con Merlín")
 
-        context.user_data['last_msg'] = reply.message_id
-    elif len(photos)>=2:
-        last_msg = context.user_data['last_msg']
+def error(update, context):
+    logger.error(tg_logger_format(update, f"Error '{context.error}' {update}"), exc_info=True)
 
-        reply_markup = generate_photo_menu(msg_id, photos, '2')
-        context.bot.edit_message_text(chat_id=chat_id, message_id=last_msg, text="¿Qué quieres hacer con estas fotos?", reply_markup=reply_markup)
-    else:
-        last_msg = context.user_data['last_msg']
-    
-        context.bot.edit_message_text(chat_id=chat_id, message_id=last_msg, text=f"Lo siento pero no soy capaz de hacer nada con {len(photos)} fotos...")
+def request(update, context):
+    msg = update.message.text
+    msg = msg[len("#Petición "):].split("\n")[0]
+    logger.info(tg_logger_format(update, f"Request '{msg}'"))
 
-    logger.info(f"({user_id}) {user_name}: {photo_id} [<<<<]")
+    update.message.reply_text(msg)
+
+
+    # chat_id = update.message.chat.id
+    # msg_id = update.message.message_id
+    # user_id = update._effective_user.id
+    # user_name = f"{update._effective_user.first_name} {update._effective_user.last_name}".replace(" None", "")
+    #
+    #     photo_id = get_photo(update)
+    # photos = get_album(update, context, photo_id)
+    #
+    # if len(photos)==1:
+    #     reply_markup = generate_photo_menu(msg_id, photos, '1')
+    #     reply = update.message.reply_text("¿Qué quieres hacer con esta foto?", reply_markup=reply_markup)
+    #
+    #     context.user_data['last_msg'] = reply.message_id
+    # elif len(photos)>=2:
+    #     last_msg = context.user_data['last_msg']
+    #
+    #     reply_markup = generate_photo_menu(msg_id, photos, '2')
+    #     context.bot.edit_message_text(chat_id=chat_id, message_id=last_msg, text="¿Qué quieres hacer con estas fotos?", reply_markup=reply_markup)
+    # else:
+    #     last_msg = context.user_data['last_msg']
+    #
+    #     context.bot.edit_message_text(chat_id=chat_id, message_id=last_msg, text=f"Lo siento pero no soy capaz de hacer nada con {len(photos)} fotos...")
+    #
+    # logger.info(f"({user_id}) {user_name}: {photo_id} [<<<<]")
 
 @run_async
 def cmd(update, context):
@@ -306,7 +301,7 @@ def cmd(update, context):
     cmd = data['cmd']
     photos_id = data['photos']
     result_id = f"{'+'.join([str(photo_id) for photo_id in photos_id])}#{cmd}"
-    
+
     check = check_photos(photos_id, context.bot) # Download again if already deleted
 
     logger.info(f"({user_id}) {user_name}: {result_id} [<---]")
@@ -383,84 +378,32 @@ def shared(update, context):
 
     logger.info(f"({user_id}) {user_name}: {result_id} [>>>>]")
 
-def error(update, context):
-    logger.warning(f"Update '{update}' caused error '{context.error}'")
-
-def bad(update, context):
-    if update._effective_chat.type=="private":
-        help(update, context)
-
 #endregion
 
 
-#region Program functions
-def run():
-    if not facade_mode:
-        global gan_methods
-        import gan_methods
-        logger.info("Loading models...")
-        gan_methods.load_models()
-        os.system('clear')
-
+def main():
     logger.info("Bot starting...")
 
     updater = Updater(tg_bot_token, use_context=True, workers=1)
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("start", start, Filters.user(user_id=tg_users())))
+    dp.add_handler(CommandHandler("help", help, Filters.user(user_id=tg_users())))
+    dp.add_handler(CommandHandler("peticion", request, Filters.user(user_id=tg_users())))
 
-    dp.add_handler(MessageHandler(Filters.photo, photo))
-    dp.add_handler(CallbackQueryHandler(cmd, pattern='^{"cmd": .*, "msg": .*, "photos": .*}$'))
-    dp.add_handler(CallbackQueryHandler(poll, pattern='^{"id": .*, "option": .*}$'))
-    dp.add_handler(InlineQueryHandler(share))
-    dp.add_handler(ChosenInlineResultHandler(shared))
+    dp.add_handler(MessageHandler(Filters.regex("#[Pp]etici[óo]n .*") & Filters.user(user_id=tg_users()), request))
+    # dp.add_handler(CallbackQueryHandler(cmd, pattern='^{"cmd": .*, "msg": .*, "photos": .*}$'))
+    # dp.add_handler(CallbackQueryHandler(poll, pattern='^{"id": .*, "option": .*}$'))
+    # dp.add_handler(InlineQueryHandler(share))
+    # dp.add_handler(ChosenInlineResultHandler(shared))
 
     dp.add_error_handler(error)
-    dp.add_handler(MessageHandler(Filters.all, bad))
+    dp.add_handler(MessageHandler(Filters.all & Filters.user(user_id=tg_users()), bad_msg))
+    dp.add_handler(MessageHandler(Filters.all, bad_user))
 
     logger.info("Bot listening...")
     updater.start_polling(timeout=60)
     updater.idle()
-
-def reset(type):
-    if not facade_mode:
-        os.system('clear')
-    
-    logger.info(f"Reseting {type}...")
-
-    if type==reset_types[1]:
-        for path in cache_paths:
-            logger.info(f"Deleting path '{path}'")
-            remove_path(path)
-    elif type==reset_types[2]:
-        for path in data_paths:
-            logger.info(f"Deleting path '{path}'")
-            remove_path(path)
-    elif type==reset_types[0]:
-        reset(reset_types[1])
-        reset(reset_types[2])
-
-#endregion
-
-
-def main():
-    parser = argparse.ArgumentParser(description="")
-    parser.add_argument("--reset", type=str, nargs='*', choices=reset_types)
-    parser.add_argument("--watermark", action='store_true', default=False)
-    parser.add_argument("--facade", action='store_true', default=False)
-    args = parser.parse_args()
-
-    global watermark_enabled
-    global facade_mode
-    watermark_enabled = args.watermark
-    facade_mode = args.facade
-    
-    if args.reset:
-        for arg in args.reset:
-            reset(arg)
-    else:
-        run()
 
 
 if __name__ == '__main__':
